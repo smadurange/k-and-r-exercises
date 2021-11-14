8cdb0c05bc3180660a6272e0b9423f66f233bc9b
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-14 17:57:59 +0800

    1.4

--- 1/3.c
+++ 1/3.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    float fahr, celsius;
-    int lower, upper, step;
+  float fahr, celsius;
+  int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-    fahr = lower;
+  lower = 0;
+  upper = 300;
+  step = 20;
+  fahr = lower;
 
-    printf("fahrenheit\tcelsius\n");
+  printf("fahrenheit\tcelsius\n");
 
-    while (fahr <= upper) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
-        printf("%10.0f\t%7.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+  while (fahr <= upper) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%10.0f\t%7.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
 
-    return 0;
+  return 0;
 }
---
+++ 1/4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  float celsius, fahr;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+  celsius = lower;
+
+  printf("celsius\tfahrenheit\n");
+
+  while (celsius <= upper) {
+    fahr = (9.0 * celsius / 5.0) + 32.0;
+    printf("%7.0f\t%10.1f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+
+  return 0;
+}
