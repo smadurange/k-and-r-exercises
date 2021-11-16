cd68f56367811e1234a5584addb7752e1de43599
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-16 18:58:27 +0800

    1.5

---
+++ 1/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Prints a conversion table from fahrenheit to celsius in reverse order */
+
+int main(int argc, char *argv[]) {
+  int fahr;
+
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+
+  return 0;
+}
