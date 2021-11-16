5433f7161662106ac6337db8e6344a705bb18fc2
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-16 19:33:57 +0800

    1.6 and 1.7

---
+++ 1/7.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    putchar(c);
+  }
+
+  printf("Value of EOF: %d\n", c);
+  return 0;
+}
