00943d404661e07370749c8a292dcd00a328db98
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-19 21:40:17 +0800

    1.16

---
+++ 1/16.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int mgetline(char s[], int lim);
+void copy(char to[], char from[]);
+
+/* prints the longest input line */
+int main(int argc, char *argv[]) {
+  int len, max;
+  char s[MAXLINE], smax[MAXLINE];
+
+  max = 0;
+
+  while ((len = mgetline(s, MAXLINE)) > 0) {
+    if (len > max) {
+      max = len;
+      copy(smax, s);
+    }
+  }
+
+  if (max) {
+    printf("%s", smax);
+    if (max == MAXLINE - 1 && smax[max] != '\n')
+      printf("\n");
+    printf("max len: %d\n", max);
+  }
+
+  return 0;
+}
+
+int mgetline(char s[], int lim) {
+  int c, i;
+
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+    s[i] = c;
+
+  if (c == '\n')
+    s[i++] = c;
+
+  s[i] = '\0';
+
+  return i;
+}
+
+void copy(char to[], char from[]) {
+  int i;
+  for (i = 0; (to[i] = from[i]) != '\0'; i++)
+    ;
+}
