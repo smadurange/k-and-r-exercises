68d16012e3dfd3d8721864d3a427dd4b56619144
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-19 19:03:35 +0800

    Comments.

--- 1/1.c
+++ 1/1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* Prints the famous "hello, world" string */
+/* prints the famous "hello, world" string */
 int main(int argc, char *argv[]) {
   printf("hello, world\n");
   return 0;
