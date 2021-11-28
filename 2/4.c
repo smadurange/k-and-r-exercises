9cfaabcb63f06fe563251cc77a8fbcc486b45773
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-28 18:37:12 +0800

    2.4

--- 2/4.c
+++ 2/4.c
@@ -31,8 +31,10 @@ void squeeze(char s1[], char s2[]) {
   for (i = j = 0; s1[i] != 0; i++) {
     delete = 0;
     for (k = 0; s2[k] != 0; k++) {
-      if (s1[i] == s2[k])
+      if (s1[i] == s2[k]) {
         delete = 1;
+        break;
+      }
     }
 
     if (!delete)
