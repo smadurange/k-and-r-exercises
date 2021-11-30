38b5d067f60d752ac5b64612356558202e12d43e
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-11-30 20:52:59 +0800

    2.7

--- 2/7.c
+++ 2/7.c
@@ -4,20 +4,27 @@
 unsigned invert(unsigned x, int p, int n);
 
 int main(int argc, char * argv[]) {
-    return 0;
+  int n, p;
+  unsigned x;
+
+  n = 3;
+  p = 5;
+  x = 0xB7;
+
+  printf("result: %x\n", invert(x, p, n));
+
+  return 0;
 }
 
 unsigned invert(unsigned x, int p, int n) {
-  unsigned xUnset, xSet, xInv;
+  unsigned mask, y;
 
   // mask to unset n bits starting at p:
-  xUnset = (~0 << p) | ~(~0 << (p - n));
+  mask = (~0 << p) | ~(~0 << (p - n));
 
-  // mask to extract bits to invert:
-  xSet = ~xUnset;
+  // invert and extract bits:
+  y = ~(x & ~mask) & ~mask;
 
-  // mask out x and copy inverted bits into unset bits:
-  xInv = (x & xUnset) | ~(x & xSet);
-
-  return xInv;
+  // mask out x and copy inverted bits into x:
+  return (x & mask) | y;
 }
