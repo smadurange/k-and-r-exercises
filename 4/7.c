3dbcb230ab5333bcf1026a9082133bd236aa6b25
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-12-12 17:06:57 +0800

    4.7

--- 4/7.c
+++ 4/7.c
@@ -38,7 +38,7 @@ double peek();
 /* clears the stack */
 void clear();
 
-/* adds support for sin, exp and pow functions */
+/* add ungets(s) to program */
 int main(int argc, char *argv[]) {
   int type, i;
   double op2, op1;
