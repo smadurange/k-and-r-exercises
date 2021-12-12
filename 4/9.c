7fe0c5231ae80dbec34414ec47fe94f85b52ccfe
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-12-12 17:07:10 +0800

    4.9

--- 4/9.c
+++ 4/9.c
@@ -38,7 +38,7 @@ double peek();
 /* clears the stack */
 void clear();
 
-/* adds support for sin, exp and pow functions */
+/* handle EOF input correctly */
 int main(int argc, char *argv[]) {
   int type, i;
   double op2, op1;
