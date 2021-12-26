b392ca9be3da36620092f33e055e59b2f8da6c54
Sadeep Madurange <smadurange@users.noreply.github.com>
2021-12-26 12:42:14 +0800

    4.13

---
+++ 4/13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 1000
+
+/* Reverses string in place using recursion */
+void reverse(char[], int, int);
+
+int main(int argc, char *argv[]) {
+  char *s;
+  size_t n;
+
+  s = malloc(MAXLEN);
+  n = sizeof(s);
+
+  getline(&s, &n, stdin);
+  reverse(s, 0, strlen(s) - 2);
+  printf("\t%s\n", s);
+
+  free(s);
+  return 0;
+}
+
+void reverse(char s[], int i, int j) {
+  if (i < j) {
+    reverse(s, i + 1, j - 1);
+    s[i] = s[i] ^ s[j];
+    s[j] = s[i] ^ s[j];
+    s[i] = s[i] ^ s[j];
+  }
+}
