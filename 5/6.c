c290e6d0bb0d65be550fe7ce075ee7cf37678c52
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-01-01 14:22:54 +0800

    5.6

---
+++ 5/6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mgetline(char *s, int lim);
+
+int mgetline(char *s, int lim) {
+  int i;
+
+  for (i = 0; i < lim - 1 && (*s++ = getchar()) != EOF && *s != '\n'; i++)
+    ;
+
+  if (*s == EOF)
+    *s = 0;
+  else
+    *++s = 0;
+
+  return i;
+}
