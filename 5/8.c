ba79458b94b9ccb3bf93015c61189e53c8176f9c
Sadeep Madurange <smadurange@users.noreply.github.com>
2022-01-04 18:48:07 +0800

    5.8

--- 5/8.c
+++ 5/8.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-/* Sets day of year from month and day */
-int day_of_year(int year, int month, int day);
-
-/* Sets month and day from day of year */
-void month_day(int year, int yearday, int *pmonth, int *pday);
-
 static char daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
 
+/* Sets day of year from month and day */
 int day_of_year(int year, int month, int day) {
   int i, leap;
 
@@ -37,6 +32,7 @@ int day_of_year(int year, int month, int day) {
   return day;
 }
 
+/* Sets month and day from day of year */
 void month_day(int year, int yearday, int *pmonth, int *pday) {
   int i, leap;
   
